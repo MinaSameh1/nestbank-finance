d410bf5bb2a89db1beb69bef2a751935b4ef604e
MinaSameh1 <minasameh1@gmail.com>
2023-11-18 10:46:45 +0200

    fixed db service

--- src/common/db/db.service.ts
+++ src/common/db/db.service.ts
@@ -1,36 +1,40 @@
 import { Injectable, Logger } from '@nestjs/common'
+import { InjectDataSource } from '@nestjs/typeorm'
 import { DataSource, EntityManager, QueryRunner } from 'typeorm'
 
 @Injectable()
 export class DatabaseService {
   private readonly logger = new Logger(DatabaseService.name)
 
+  @InjectDataSource()
   private readonly dataSource: DataSource
 
-  getDataSource(): DataSource {
+  protected getDataSource(): DataSource {
     return this.dataSource
   }
 
-  async getQueryRunner() {
+  public async getQueryRunner() {
     const queryRunner = this.dataSource.createQueryRunner()
     await queryRunner.connect()
     await queryRunner.startTransaction()
     return queryRunner
   }
 
-  commitTransaction(queryRunner: QueryRunner) {
+  public commitTransaction(queryRunner: QueryRunner) {
     return queryRunner.commitTransaction()
   }
 
-  rollbackTransaction(queryRunner: QueryRunner) {
+  public rollbackTransaction(queryRunner: QueryRunner) {
     return queryRunner.rollbackTransaction()
   }
 
-  releaseQueryRunner(queryRunner: QueryRunner) {
+  public releaseQueryRunner(queryRunner: QueryRunner) {
     return queryRunner.release()
   }
 
-  doInTransaction<T>(callback: (entityManager: EntityManager) => Promise<T>) {
+  public doInTransaction<T>(
+    callback: (entityManager: EntityManager) => Promise<T>,
+  ) {
     return this.dataSource.transaction(callback)
   }
 }
